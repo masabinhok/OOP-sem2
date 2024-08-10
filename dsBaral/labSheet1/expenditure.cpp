7244470ab02ff3a4e844e068d7e366daab68667c
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-08-10 18:38:53 +0545

    fixed some error

--- dsBaral/labSheet1/expenditure.cpp
+++ dsBaral/labSheet1/expenditure.cpp
@@ -1,142 +1,137 @@
 #include <iostream>
 using namespace std;
 
-//declaring functions
-void handleTask(int expenditure[][12], int num_years);
+// Function declarations
 void calcMonthlyAvg(int expenditure[][12], int num_years);
 void calcYearlyAvg(int expenditure[][12], int num_years);
 void calcRangeAvg(int expenditure[][12], int num_years);
+void addData(int expenditure[][12], int num_years);
 
-void handleTask(int expenditure[][12], int num_years){
-
-  cout<< "Here's a list of task can perform:"<< endl;
-  cout<< "1. Monthly Average"<<endl;
-  cout<< "2. Yearly Average"<<endl;
-  cout<< "3. Average over a range of time"<<endl;
-
-  cout<<"Enter 1, 2, or 3 to perform respective task"<< endl;
-  
-  int task;
-  cin>>task;
-
-
-  switch (task)
-  {
-  case 1: calcMonthlyAvg(expenditure, num_years);
-    break;
-
-
-  case 2: calcYearlyAvg(expenditure, num_years);
-  break;
-
-  case 3: calcRangeAvg(expenditure, num_years);
-  break;
-
-  default:
-    cout<<"Please follow the instructions properly!"<<endl;
-    break;
-  }
-}
-
-void calcMonthlyAvg(int expenditure[][12], int num_years){
-  int year, month;
-  cout<<"You have to provide me specific data for which month of which year to get the data."<<endl;
-  cout<<"Year: "<<endl;
-  cin>>year;
-  cout<<"Month: "<<endl;
-  cin>>month;
-
-  year--;
-  month--;
-  cout<<"The monthly average for the year "<<year+1<<" month "<<month+1<< " is: "<< expenditure[year][month]<<endl;
-  handleTask(expenditure, num_years) ;
-}
-
-void calcYearlyAvg(int expenditure[][12],int num_years){
-  int year;
-  cout<<"For which year, you want to calculate the average? \t";
-  cin>>year;
-  year--;
-  int yearlySum = 0;
-
-    for(int j =0; j<12; j++){
-      yearlySum+= expenditure[year][j];
+void addData(int expenditure[][12], int num_years) {
+    for (int i = 0; i < num_years; i++) {
+        for (int j = 0; j < 12; j++) {
+            cout << "Enter the expenditure for year " << i + 1 << " month " << j + 1 << ": ";
+            cin >> expenditure[i][j];
+        }
     }
-  int yearlyAvg;
-  yearlyAvg = yearlySum/12; 
-  year++;
-  cout<<"The total expenditure is: "<<yearlySum<<endl;
-  cout<<"The yearly average for the year "<<year<<" is: "<<yearlyAvg<<endl;
-
-  handleTask(expenditure, num_years) ;
+    cout << "Thank you for entering the data. Record Kept!" << endl;
 }
 
+void calcMonthlyAvg(int expenditure[][12], int num_years) {
+    int year, month;
+    cout << "You have to provide specific data for which month of which year to get the average." << endl;
+    cout << "Year: ";
+    cin >> year;
+    cout << "Month: ";
+    cin >> month;
 
-void calcRangeAvg(int expenditure[][12], int num_years){
-  int initial_year, initial_month, final_year, final_month;
-  int month_count=0;
-  
-  cout<<"So you want to calculate average over a range of years and months?? huh! For that you have to give me initial year, month and final year, month. You down??"<<endl;
+    year--;
+    month--;
 
-  cout<<"Initial Year: \t"; cin>>initial_year;
-  cout<<"Initial Month: \t"; cin>>initial_month;
-  cout<<"Final Year: \t"; cin>>final_year;
-  cout<<"Final Month: \t"; cin>>final_month;
-
-  //array index milauna minus gareko
-  initial_year--;
-  initial_month--;
-  final_year--;
-  final_month--;
-
-  int rangeSum = 0;
-  
-  for(int i = initial_year; i<=final_year; i++){
-
-    int start_month = (i== initial_year? initial_month==initial_month: 0);
-    int end_month = (i==final_year? final_month==final_month: 11);
-    for(int j=start_month; j<=end_month; j++){
-      rangeSum += expenditure[i][j];
-      month_count++;
+    if (year < num_years && month < 12) {
+        cout << "The expenditure for year " << year + 1 << " month " << month + 1 << " is: " << expenditure[year][month] << endl;
+    } else {
+        cout << "Invalid year or month. Please try again." << endl;
     }
-  }
-
-  int rangeAvg;
-  rangeAvg = rangeSum/month_count;
-
-  cout<<"The total expenditure is: "<<rangeSum<<endl;
-  cout<<"The avg expenditure per month is: "<<rangeAvg<<endl;
-
-
-    handleTask(expenditure, num_years);
-  }
-
-  
-
-
-
-
-
-
-int main () {
-  int num_years;
-  cout<<"Enter the number of years to track expenses"<<endl;
-  cin>>num_years;
-  int expenditure[num_years][12];
-
-  for(int i = 0; i<num_years; i++){
-    for(int j =0; j<12; j++){
-      cout << "Enter the expenditure for year " << i+1 << " month " << j+1 << endl;
-      cin>>expenditure[i][j];
-    }
-  }
-
-  cout<<"Thank you for entering the data. Record Kept!"<<endl;
-
-  handleTask(expenditure, num_years);
-
-  return 0;
 }
 
+void calcYearlyAvg(int expenditure[][12], int num_years) {
+    int year;
+    cout << "For which year do you want to calculate the average? ";
+    cin >> year;
+    year--;
 
+    if (year < num_years) {
+        int yearlySum = 0;
+        for (int j = 0; j < 12; j++) {
+            yearlySum += expenditure[year][j];
+        }
+        int yearlyAvg = yearlySum / 12;
+        cout << "The total expenditure is: " << yearlySum << endl;
+        cout << "The yearly average for year " << year + 1 << " is: " << yearlyAvg << endl;
+    } else {
+        cout << "Invalid year. Please try again." << endl;
+    }
+}
 
+void calcRangeAvg(int expenditure[][12], int num_years) {
+    int initial_year, initial_month, final_year, final_month;
+    int month_count = 0;
+
+    cout << "Enter the initial year and month, followed by the final year and month for the range." << endl;
+    cout << "Initial Year: ";
+    cin >> initial_year;
+    cout << "Initial Month: ";
+    cin >> initial_month;
+    cout << "Final Year: ";
+    cin >> final_year;
+    cout << "Final Month: ";
+    cin >> final_month;
+
+    initial_year--;
+    initial_month--;
+    final_year--;
+    final_month--;
+
+    int rangeSum = 0;
+
+    for (int i = initial_year; i <= final_year; i++) {
+        int start_month = (i == initial_year ? initial_month : 0);
+        int end_month = (i == final_year ? final_month : 11);
+
+        for (int j = start_month; j <= end_month; j++) {
+            rangeSum += expenditure[i][j];
+            month_count++;
+        }
+    }
+
+    if (month_count > 0) {
+        int rangeAvg = rangeSum / month_count;
+        cout << "The total expenditure over the range is: " << rangeSum << endl;
+        cout << "The average expenditure per month over the range is: " << rangeAvg << endl;
+    } else {
+        cout << "Invalid range. Please try again." << endl;
+    }
+}
+
+int main() {
+    int num_years;
+    cout << "Enter the number of years to track expenses: ";
+    cin >> num_years;
+
+    int expenditure[num_years][12];
+    addData(expenditure, num_years);
+
+    int task = 0;
+
+    while (task != 69) {
+        cout << "\nHere's a list of tasks you can perform:" << endl;
+        cout << "1. Monthly Expenditure" << endl;
+        cout << "2. Yearly Average" << endl;
+        cout << "3. Average over a range of time" << endl;
+        cout << "69. Exit" << endl;
+
+        cout << "Enter your choice: ";
+        cin >> task;
+
+        switch (task) {
+            case 1:
+                calcMonthlyAvg(expenditure, num_years);
+                break;
+            case 2:
+                calcYearlyAvg(expenditure, num_years);
+                break;
+            case 3:
+                calcRangeAvg(expenditure, num_years);
+                break;
+            case 69:
+                cout << "Exiting the program." << endl;
+                break;
+            default:
+                cout << "Please enter a valid choice!" << endl;
+                break;
+        }
+    }
+
+    return 0;
+}
